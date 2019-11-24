1ec13739a7167f0dc411eadc78fe86ee46ab9ead
Carlos Felipe Domingues e Oliveira <11181378+CarlosDomingues@users.noreply.github.com>
2019-11-24 17:13:21 -0300

    Update devx.ps1

--- devx.ps1
+++ devx.ps1
@@ -7,110 +7,86 @@ echo "Configuring Windows 10 and installing neat programs for software developme
 # Installs Scoop and aria2 for faster downloads.
 # Scoop is a package manager for Windows.
 # Packages are installed under ~/scoop
-###################################################
-function Install-Scoop
-{
-  Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
-  scoop install aria2 
-  
-}
+##################################################
+Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install aria2 
 
 # Installs tools commonly present in most 
 # Linux distributions.
 ###################################################
-function Install-LinuxTools
-{
-  scoop install bind `
-                cacert `
-                curl `
-                coreutils `
-                dig `
-                dos2unix `
-                ffmpeg `
-                gawk `
-                git `
-                gpg `
-                graphviz `
-                grep `
-                gzip `
-                iperf3 `
-                jq `
-                less `
-                nano `
-                netcat `
-                openssh `
-                openssl `
-                pandoc `
-                png2jpeg `
-                pwsh `
-                sed `
-                shasum `
-                shellcheck `
-                sudo `
-                tar `
-                telnet `
-                time `
-                touch `
-                unrar `
-                unzip `
-                vim `
-                wget `
-                which `
-                youtube-dl
-}
+scoop install bind `
+              cacert `
+              curl `
+              coreutils `
+              dig `
+              dos2unix `
+              ffmpeg `
+              gawk `
+              git `
+              gpg `
+              graphviz `
+              grep `
+              gzip `
+              iperf3 `
+              jq `
+              less `
+              nano `
+              netcat `
+              openssh `
+              openssl `
+              pandoc `
+              png2jpeg `
+              pwsh `
+              sed `
+              shasum `
+              shellcheck `
+              sudo `
+              tar `
+              telnet `
+              time `
+              touch `
+              unrar `
+              unzip `
+              vim `
+              wget `
+              which `
+              youtube-dl
 
 # Installs programming languages and related
 # tools.
 ###################################################
-function Install-DeveloperTools
-{
-  scoop install dotnet-sdk `
-                go `
-                hadolint `
-                leiningen `
-                gcc `
-                make `
-                nodejs `
-                nuget `
-                python `
-                rustup `
-                sbt `
-                shellcheck `
-                shfmt
-}
+scoop install dotnet-sdk `
+              go `
+              hadolint `
+              leiningen `
+              gcc `
+              make `
+              nodejs `
+              nuget `
+              python `
+              rustup `
+              sbt `
+              shellcheck `
+              shfmt
 
 # Installs cloud infrastructure related tools,
 # languages and frameworks.
 ###################################################
-function Install-InfraTools
-{
-  scoop install aws `
-                consul `
-                nomad `
-                packer `
-                terraform `
-                terragrunt `
-                vault
-}
+scoop install aws `
+              consul `
+              nomad `
+              packer `
+              terraform `
+              terragrunt `
+              vault
 
 # Configure Windows Explorer to show file
 # extension, hidden files and disable 
 # path limits.
 ###################################################
-function Set-ExplorerConfiguration
-{
-  $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-  Set-ItemProperty $key Hidden 1
-  Set-ItemProperty $key HideFileExt 0
-  Set-ItemProperty $key ShowSuperHidden 1
-  Stop-Process -processname explorer
-  sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
-}
-
-# Install-Scoop
-Install-LinuxTools
-# Install-DevTools
-# Install-InfraTools
-# Set-ExplorerConfiguration
-
-
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 1
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key ShowSuperHidden 1
+Stop-Process -processname explorer
+sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
